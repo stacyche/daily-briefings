e3a5a6c7e19d483cd90e11839b9a0894ea651ab0
stacyche <xc172@georgetown.edu>
2022-04-11 18:14:28 -0400

    routes change: book, home, weather

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,4 +1,6 @@
 
+
+
 from flask import Blueprint, request, render_template
 
 home_routes = Blueprint("home_routes", __name__)
@@ -15,3 +17,22 @@ def about():
     print("ABOUT...")
     return "About Me"
     #return render_template("about.html")
+
+@home_routes.route("/another")
+def another():
+    print("ANOTHER PAGE MAYBE...")
+    return "Here is another page"
+
+
+@home_routes.route("/hello")
+def hello_world():
+    print("HELLO...", dict(request.args))
+ 
+    #go check the URL params for the one called "name", and use it if possible
+    #if no "name" parameter is specified, use a default value 
+    name = request.args.get("name") or "World"
+
+
+    message = f"Hello, {name}!"
+    return message
+    #return render_template("hello.html", message=message)
