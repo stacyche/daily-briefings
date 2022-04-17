39f58a564f406fa38a7be46c58c69f681359b2d3
stacyche <xc172@georgetown.edu>
2022-04-17 19:50:54 -0400

    Bootstrap Layout

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -27,6 +27,11 @@ def another():
 @home_routes.route("/hello")
 def hello_world():
     print("HELLO...", dict(request.args))
+
+    # if the request contains url params, for example a request to "/hello?name=Harper"
+    # the request object's args property will hold the values in a dictionary-like structure
+    url_params = dict(request.args)
+    print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
  
     #go check the URL params for the one called "name", and use it if possible
     #if no "name" parameter is specified, use a default value 
